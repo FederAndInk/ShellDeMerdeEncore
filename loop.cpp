6ba850dc92dd73ab986724de95b0a2aff328932a
FederAndInk <federandink@gmail.com>
2019-02-06 18:29:14 +0100

    format

---
+++ loop.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <limits>
+
+int main(int argc, char const* argv[])
+{
+  int i;
+  std::cin.exceptions(std::istream::badbit | std::istream::failbit);
+  while (1)
+  {
+    try
+    {
+      std::cin >> i;
+      std::cout << i << "\n";
+    }
+    catch (std::ios_base::failure const& e)
+    {
+      std::cout << e.what() << "\n";
+      std::cout << e.code() << "\n";
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "Wrong number\n";
+    }
+  }
+  return 0;
+}
--- src/shell.c
+++ src/shell.c
@@ -16,17 +16,23 @@
 
 void prompt()
 {
-    printf("shell> ");
-    fflush(stdout);
+  printf("shell> ");
+  fflush(stdout);
 }
 
 void exitShell()
 {
-    printf("exit\n");
-    exit(0);
+  printf("exit\n");
+  exit(0);
 }
 
-void handleStatus(int status) {}
+void handleStatus(int status)
+{
+  if (WTERMSIG(status) == SIGINT)
+  {
+    printf("\n");
+  }
+}
 
 int const WRITE = O_CREAT | O_WRONLY;
 int const READ = O_RDONLY;
@@ -34,114 +40,120 @@ int const NO_FILE = -1;
 
 int openIn(char const* path, int mod)
 {
-    if (!path)
+  if (!path)
+  {
+    return NO_FILE;
+  }
+  else
+  {
+    int desc = open(path, mod, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    if (desc >= 0)
     {
-        return NO_FILE;
+      return desc;
     }
     else
     {
-        int desc = open(path, mod, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-        if (desc >= 0)
-        {
-            return desc;
-        }
-        else
-        {
-            perror(path);
-            exit(1);
-        }
+      perror(path);
+      exit(1);
     }
+  }
 }
 
 void callExec(CmdLine cmdL, int no)
 {
-    char** cmd = cmdL->seq[no];
-    signal(SIGINT, SIG_DFL);
-    if (execvp(*cmd, cmd) < 0)
-    {
-        fprintf(stderr, "command not found: %s\n", *cmd);
-        exit(1);
-    }
+  char** cmd = cmdL->seq[no];
+  signal(SIGINT, SIG_DFL);
+  if (execvp(*cmd, cmd) < 0)
+  {
+    fprintf(stderr, "command not found: %s\n", *cmd);
+    exit(1);
+  }
 }
 
 void execSubCommand(CmdLine cmdL)
 {
-    // No pipe
-    if (cmdL->seq[1] == NULL)
+  // No pipe
+  if (cmdL->seq[1] == NULL)
+  {
+    pid_t pid = fork();
+    if (pid == 0)
     {
-        pid_t pid = fork();
-        if (pid == 0)
-        {
-            // For the first process in the pipe
-            int in = openIn(cmdL->in, READ);
-            if (in >= 0)
-            {
-                dup2(in, STDIN_FILENO);
-            }
+      // For the first process in the pipe
+      int in = openIn(cmdL->in, READ);
+      if (in >= 0)
+      {
+        dup2(in, STDIN_FILENO);
+        close(in);
+      }
 
-            // For the last process in the pipe
-            int out = openIn(cmdL->out, WRITE);
-            if (out >= 0)
-            {
-                dup2(out, STDOUT_FILENO);
-            }
-            callExec(cmdL, 0);
-        }
-        else
-        {
-            signal(SIGINT, SIG_IGN);
-            int status;
-            wait(&status);
-            handleStatus(status);
-            signal(SIGINT, ctrlCHandler);
-        }
+      // For the last process in the pipe
+      int out = openIn(cmdL->out, WRITE);
+      if (out >= 0)
+      {
+        dup2(out, STDOUT_FILENO);
+        close(out);
+      }
+      callExec(cmdL, 0);
     }
-    // With pipe(s)
     else
     {
-        int pipeDesc[2];
-        pipe(pipeDesc);
-
-        pid_t pid = fork();
-        if (pid == 0)
-        {
-            // Redirect input file for the first process in the pipe
-            int in = openIn(cmdL->in, READ);
-            if (in >= 0)
-            {
-                dup2(in, STDIN_FILENO);
-            }
-            close(pipeDesc[PIPE_READ]);
-            dup2(pipeDesc[PIPE_WRITE], STDOUT_FILENO);
-            callExec(cmdL, 0);
-        }
-        else
-        {
-            pid_t pid = fork();
-            if (pid == 0)
-            {
-                // Redirect output file for the last process in the pipe
-                int out = openIn(cmdL->out, WRITE);
-                if (out >= 0)
-                {
-                    dup2(out, STDOUT_FILENO);
-                }
-                close(pipeDesc[PIPE_WRITE]);
-                dup2(pipeDesc[PIPE_READ], STDIN_FILENO);
-                callExec(cmdL, 1);
-            }
-            else
-            {
-                close(pipeDesc[PIPE_READ]);
-                close(pipeDesc[PIPE_WRITE]);
-                signal(SIGINT, SIG_IGN);
-                int status;
-                wait(&status);
-                handleStatus(status);
-                signal(SIGINT, ctrlCHandler);
-            }
-        }
+      signal(SIGINT, SIG_IGN);
+      int status;
+      wait(&status);
+      handleStatus(status);
+      signal(SIGINT, ctrlCHandler);
     }
+  }
+  // With pipe(s)
+  else
+  {
+    int pipeDesc[2];
+    pipe(pipeDesc);
+
+    pid_t pid = fork();
+    if (pid == 0)
+    {
+      // Redirect input file for the first process in the pipe
+      int in = openIn(cmdL->in, READ);
+      if (in >= 0)
+      {
+        dup2(in, STDIN_FILENO);
+        close(in);
+      }
+      close(pipeDesc[PIPE_READ]);
+      dup2(pipeDesc[PIPE_WRITE], STDOUT_FILENO);
+      close(pipeDesc[PIPE_WRITE]);
+      callExec(cmdL, 0);
+    }
+    else
+    {
+      pid_t pid = fork();
+      if (pid == 0)
+      {
+        // Redirect output file for the last process in the pipe
+        int out = openIn(cmdL->out, WRITE);
+        if (out >= 0)
+        {
+          dup2(out, STDOUT_FILENO);
+          close(out);
+        }
+        close(pipeDesc[PIPE_WRITE]);
+        dup2(pipeDesc[PIPE_READ], STDIN_FILENO);
+        close(pipeDesc[PIPE_READ]);
+        callExec(cmdL, 1);
+      }
+      else
+      {
+        close(pipeDesc[PIPE_READ]);
+        close(pipeDesc[PIPE_WRITE]);
+        signal(SIGINT, SIG_IGN);
+        int status;
+        wait(&status);
+        handleStatus(status);
+        signal(SIGINT, ctrlCHandler);
+      }
+    }
+  }
 }
 
 /**
@@ -151,68 +163,69 @@ void execSubCommand(CmdLine cmdL)
  */
 void processCommands(CmdLine l)
 {
-    char** cmd = l->seq[0];
+  char** cmd = l->seq[0];
 
-    // Internal commands
-    if (strcmp("quit", cmd[0]) == 0)
-    {
-        exitShell();
-    }
-    else if (strcmp("clear", cmd[0]) == 0)
-    {
-        printf("clearing\n");
-        execSubCommand(l);
-    }
-    // Other commands
-    else
-    {
-        execSubCommand(l);
-    }
+  // Internal commands
+  if (strcmp("quit", cmd[0]) == 0)
+  {
+    exitShell();
+  }
+  else if (strcmp("clear", cmd[0]) == 0)
+  {
+    printf("clearing\n");
+    execSubCommand(l);
+  }
+  // Other commands
+  else
+  {
+    execSubCommand(l);
+  }
 }
 
 int main()
 {
-    signal(SIGCHLD, childHandler);
+  signal(SIGCHLD, childHandler);
 
-    while (1)
+  while (1)
+  {
+    struct cmdline* l;
+    int             i, j;
+
+    // block ctrl+c signal
+    signal(SIGINT, ctrlCHandler);
+
+    prompt();
+    l = readcmd();
+
+    /* If input stream closed, normal termination */
+    if (!l)
     {
-        struct cmdline* l;
-        int             i, j;
-
-        // block ctrl+c signal
-        signal(SIGINT, ctrlCHandler);
-
-        prompt();
-        l = readcmd();
-
-        /* If input stream closed, normal termination */
-        if (!l)
-        {
-            exitShell();
-        }
-
-        if (l->seq[0] == NULL) {
-            continue;
-        }
-
-        if (l->err)
-        {
-            /* Syntax error, read another command */
-            printf("error: %s\n", l->err);
-            continue;
-        }
-
-        for (i = 0; l->seq[i] != NULL; i++)
-        {
-            char** cmd = l->seq[i];
-            printf("seq[%d]: ", i);
-            for (j = 0; cmd[j] != 0; j++)
-            {
-                printf("%s ", cmd[j]);
-            }
-            printf("\n");
-        }
-
-        processCommands(l);
+      exitShell();
     }
+
+    if (l->seq[0] == NULL)
+    {
+      continue;
+    }
+
+    if (l->err)
+    {
+      /* Syntax error, read another command */
+      printf("error: %s\n", l->err);
+      continue;
+    }
+
+    for (i = 0; l->seq[i] != NULL; i++)
+    {
+      char** cmd = l->seq[i];
+      printf("seq[%d]: ", i);
+      for (j = 0; cmd[j] != 0; j++)
+      {
+        printf("%s ", cmd[j]);
+      }
+      printf("\n");
+    }
+
+    processCommands(l);
+  }
 }
