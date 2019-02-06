5f6afc45c622464b71b00853971a7ec8dd08b31b
Arckaren <axelj38@gmail.com>
2019-02-06 15:22:44 +0100

    fnrbdhg

--- src/shell.c
+++ src/shell.c
@@ -13,14 +13,14 @@
 
 void prompt()
 {
-  printf("shell> ");
-  fflush(stdout);
+    printf("shell> ");
+    fflush(stdout);
 }
 
 void exitShell()
 {
-  printf("exit\n");
-  exit(0);
+    printf("exit\n");
+    exit(0);
 }
 
 void handleStatus(int status) {}
@@ -31,109 +31,113 @@ int const NO_FILE = -1;
 
 int openIn(char const* path, int mod)
 {
-  if (!path)
-  {
-    return NO_FILE;
-  }
-  else
-  {
-    int desc = open(path, mod, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-    if (desc >= 0)
+    if (!path)
     {
-      return desc;
+        return NO_FILE;
     }
     else
     {
-      perror(path);
-      exit(1);
+        int desc = open(path, mod, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+        if (desc >= 0)
+        {
+            return desc;
+        }
+        else
+        {
+            perror(path);
+            exit(1);
+        }
     }
-  }
 }
 
-void execSubCommand(char** cmd, int in, int out)
+void execSubCommand(CmdLine cmdL)
 {
-  pid_t pid = fork();
-  if (pid == 0)
-  {
-    if (out >= 0)
+    pid_t pid = fork();
+    if (pid == 0)
     {
-      dup2(out, STDOUT_FILENO);
-    }
+        // For the first process in the pipe
+        int in = openIn(cmdL->in, READ);
+        if (in >= 0)
+        {
+            dup2(in, STDIN_FILENO);
+        }
 
-    if (in >= 0)
-    {
-      dup2(in, STDIN_FILENO);
+        // For the last process in the pipe
+        int out = openIn(cmdL->out, WRITE);
+        if (out >= 0)
+        {
+            dup2(out, STDOUT_FILENO);
+        }
+        char** cmd = cmdL->seq[0];
+        signal(SIGINT, SIG_DFL);
+        if (execvp(*cmd, cmd) < 0)
+        {
+            fprintf(stderr, "command not found: %s\n", *cmd);
+            exit(1);
+        }
     }
-
-    signal(SIGINT, SIG_DFL);
-    if (execvp(*cmd, cmd) < 0)
+    else
     {
-      fprintf(stderr, "command not found: %s\n", *cmd);
-      exit(1);
+        signal(SIGINT, SIG_IGN);
+        int status;
+        wait(&status);
+        handleStatus(status);
+        signal(SIGINT, ctrlCHandler);
     }
-  }
-  else
-  {
-    signal(SIGINT, SIG_IGN);
-    int status;
-    wait(&status);
-    handleStatus(status);
-    signal(SIGINT, ctrlCHandler);
-  }
 }
 
 /**
  * @brief process internal commands
- * 
- * @param cmd 
+ *
+ * @param cmd
  */
 void processCommands(CmdLine l)
 {
-  char** cmd = l->seq[0];
+    char** cmd = l->seq[0];
 
-  // Internal commands
-  if (strcmp("quit", cmd[0]) == 0)
-  {
-    exitShell();
-  }
-  else if (strcmp("clear", cmd[0]) == 0)
-  {
-    printf("clearing\n");
-    execSubCommand(cmd, NO_FILE, NO_FILE);
-  }
-  // Other commands
-  else
-  {
-    execSubCommand(cmd, openIn(l->in, READ), openIn(l->out, WRITE));
-  }
+    // Internal commands
+    if (strcmp("quit", cmd[0]) == 0)
+    {
+        exitShell();
+    }
+    else if (strcmp("clear", cmd[0]) == 0)
+    {
+        printf("clearing\n");
+        execSubCommand(l);
+    }
+    // Other commands
+    else
+    {
+        execSubCommand(l);
+    }
 }
 
 int main()
 {
-  while (1)
-  {
-    struct cmdline* l;
-    int             i, j;
-
-    // block ctrl+c signal
-    signal(SIGINT, ctrlCHandler);
-
-    prompt();
-    l = readcmd();
-
-    /* If input stream closed, normal termination */
-    if (!l)
+    while (1)
     {
-      exitShell();
-    }
+        struct cmdline* l;
+        int             i, j;
 
-    if (l->err)
-    {
-      /* Syntax error, read another command */
-      printf("error: %s\n", l->err);
-      continue;
-    }
+        // block ctrl+c signal
+        signal(SIGINT, ctrlCHandler);
 
-    processCommands(l);
-  }
+        prompt();
+        l = readcmd();
+
+        /* If input stream closed, normal termination */
+        if (!l)
+        {
+            exitShell();
+        }
+
+        if (l->err)
+        {
+            /* Syntax error, read another command */
+            printf("error: %s\n", l->err);
+            continue;
+        }
+
+        processCommands(l);
+    }
 }
